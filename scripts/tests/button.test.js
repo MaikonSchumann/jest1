34e848c78d8fd21a4ab0f3601911916ae359f97d
MaikonSchumann <71983707+MaikonSchumann@users.noreply.github.com>
2024-04-15 09:49:27 +0100

    final tests

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,12 +5,23 @@
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = '<p id="par"></p>';
+    // This will test only the p element on DOM
+    // document.body.innerHTML = '<p id="par"></p>';
+
+    // This will test the entire DOM
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('index.html', 'utf-8');
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe('DOM tests', () => {
     test('expect p content to change', () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toBe("You Clicked");
-    })
-})
+    });
+    test('h1 should exist', () => {
+        expect(document.getElementsByTagName('h1').length).toBe(1);
+    });
+});
